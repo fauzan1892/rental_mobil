b5db140150f1284f418688ad7bfe8e17ecf89342
Fauzan Falah <fauzancodekop@gmail.com>
2023-06-26 00:32:20 +0700

    update

--- admin/mobil/proses.php
+++ admin/mobil/proses.php
@@ -20,14 +20,23 @@ if (empty($_SESSION['USER'])) {
 }
 
 if ($_GET['aksi'] == 'tambah') {
-    $dir = '../../assets/image/';
-    $tmp_name = $_FILES['gambar']['tmp_name'];
-    $temp = explode(".", $_FILES["gambar"]["name"]);
-    $newfilename = round(microtime(true)) . '.' . end($temp);
-    $target_path = $dir . basename($newfilename);
-    $allowedImageType = array("image/gif",   "image/JPG",   "image/jpeg",   "image/pjpeg",   "image/png",   "image/x-png"  );
 
-    if ($_FILES['gambar']["error"] > 0) {
+    $allowedImageType = array("image/gif", "image/JPG", "image/jpeg", "image/pjpeg", "image/png", "image/x-png", 'image/webp');
+    $filepath = $_FILES['gambar']['tmp_name'];
+    $fileSize = filesize($filepath);
+    $fileinfo = finfo_open(FILEINFO_MIME_TYPE);
+    $filetype = finfo_file($fileinfo, $filepath);
+    $allowedTypes = [
+        'image/png'   => 'png',
+        'image/jpeg'  => 'jpg',
+        'image/gif'   => 'gif',
+        'image/jpg'   => 'jpeg',
+        'image/webp'  => 'webp'
+    ];
+    if(!in_array($filetype, array_keys($allowedTypes))) {
+        echo '<script>alert("You can only upload JPG, PNG and GIF file");window.location="tambah.php"</script>';
+        exit();
+    }else if ($_FILES['gambar']["error"] > 0) {
         echo '<script>alert("Error file");history.go(-1)</script>';
         exit();
     } elseif (!in_array($_FILES['gambar']["type"], $allowedImageType)) {
@@ -37,6 +46,11 @@ if ($_GET['aksi'] == 'tambah') {
         echo '<script>alert("WARNING !!! Besar Gambar Tidak Boleh Lebih Dari 4 MB !");window.location="tambah.php"</script>';
         exit();
     } else {
+        $dir = '../../assets/image/';
+        $tmp_name = $_FILES['gambar']['tmp_name'];
+        $temp = explode(".", $_FILES["gambar"]["name"]);
+        $newfilename = round(microtime(true)) . '.' . end($temp);
+        $target_path = $dir . basename($newfilename);
         if (move_uploaded_file($tmp_name, $target_path)) {
             $data[] = $_POST['no_plat'];
             $data[] = $_POST['merk'];
@@ -57,12 +71,6 @@ if ($_GET['aksi'] == 'tambah') {
 }
 
 if ($_GET['aksi'] == 'edit') {
-    $dir = '../../assets/image/';
-    $tmp_name = $_FILES['gambar']['tmp_name'];
-    $temp = explode(".", $_FILES["gambar"]["name"]);
-    $newfilename = round(microtime(true)) . '.' . end($temp);
-    $target_path = $dir . basename($newfilename);
-    $allowedImageType = array("image/gif",   "image/JPG",   "image/jpeg",   "image/pjpeg",   "image/png",   "image/x-png"  );
 
     $gambar = $_POST['gambar_cek'];
 
@@ -73,7 +81,22 @@ if ($_GET['aksi'] == 'edit') {
     $data[] = $_POST['harga'];
     $data[] = $_POST['deskripsi'];
     $data[] = $_POST['status'];
-    if ($_FILES['gambar']["size"] > 0) {
+    $allowedImageType = array("image/gif", "image/JPG", "image/jpeg", "image/pjpeg", "image/png", "image/x-png", 'image/webp');
+    $filepath = $_FILES['gambar']['tmp_name'];
+    $fileSize = filesize($filepath);
+    $fileinfo = finfo_open(FILEINFO_MIME_TYPE);
+    $filetype = finfo_file($fileinfo, $filepath);
+    $allowedTypes = [
+        'image/png'   => 'png',
+        'image/jpeg'  => 'jpg',
+        'image/gif'   => 'gif',
+        'image/jpg'   => 'jpeg',
+        'image/webp'  => 'webp'
+    ];
+    if(!in_array($filetype, array_keys($allowedTypes))) {
+        echo '<script>alert("You can only upload JPG, PNG and GIF file");window.location="tambah.php"</script>';
+        exit();
+    }else if ($_FILES['gambar']["size"] > 0) {
         if ($_FILES['gambar']["error"] > 0) {
             echo '<script>alert("Error file");history.go(-1)</script>';
             exit();
@@ -84,6 +107,11 @@ if ($_GET['aksi'] == 'edit') {
             echo '<script>alert("WARNING !!! Besar Gambar Tidak Boleh Lebih Dari 4 MB !");history.go(-1)</script>';
             exit();
         } else {
+            $dir = '../../assets/image/';
+            $tmp_name = $_FILES['gambar']['tmp_name'];
+            $temp = explode(".", $_FILES["gambar"]["name"]);
+            $newfilename = round(microtime(true)) . '.' . end($temp);
+            $target_path = $dir . basename($newfilename);
             if (move_uploaded_file($tmp_name, $target_path)) {
                 if (file_exists('../../assets/image/'.$gambar)) {
                     unlink('../../assets/image/'.$gambar);
